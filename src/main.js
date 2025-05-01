bfd11399b0bb311c4a8c90a4b5025b92632f1487
Aleksey Beletsky <53120159+Beleks@users.noreply.github.com>
2025-05-01 20:14:29 +0800

    Create compute shader

--- src/main.js
+++ src/main.js
@@ -38,9 +38,13 @@ getDevice()
     setContextConfig();
 
     const module = initModule(device);
-    const pipeline = initPipeline(device, module);
+    const moduleCompute = initComputedModule(device)
 
-    render(device, pipeline);
+    const pipelineRender = initRenderPipeline(device, module);
+    const pipelineCompute = initComputePipeline(device, moduleCompute);
+
+    render(device, pipelineRender);
+    renderCompute(device, pipelineCompute);
   })
   .catch((error) => {
     console.error(error);
@@ -87,7 +91,18 @@ function initComputedModule(device) {
   });
 }
 
-function initPipeline(device, module) {
+function initComputePipeline(device, module) {
+  return device.createComputePipeline({
+    label: "doubling compute module",
+    layout: 'auto',
+    // language=wgsl
+    compute: {
+      module
+    },
+  });
+}
+
+function initRenderPipeline(device, module) {
   return device.createRenderPipeline({
     label: "our hardcoded red triangle pipeline",
     layout: "auto",
@@ -138,5 +153,56 @@ function render(device, pipeline) {
   const commandBuffer = encoder.finish();
   device.queue.submit([commandBuffer]);
 }
+
+async function renderCompute(device, pipeline) {
+  const input = new Float32Array([1, 3, 5]);
+
+  const workBuffer = device.createBuffer({
+    label: 'work buffer',
+    size: input.byteLength,
+    // Разробраться в usage?
+    usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC | GPUBufferUsage.COPY_DST,
+  });
+
+  device.queue.writeBuffer(workBuffer, 0, input);
+
+  const resultBuffer = device.createBuffer({
+    label: 'result buffer',
+    size: input.byteLength,
+    usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST,
+  });
+
+  const bindGroup = device.createBindGroup({
+    label: 'bindGroup for work buffer',
+    layout: pipeline.getBindGroupLayout(0),
+    entries: [
+      { binding: 0, resource: { buffer: workBuffer } },
+    ],
+  });
+
+  const encoder = device.createCommandEncoder({
+    label: 'doubling encoder',
+  });
+  const pass = encoder.beginComputePass({
+    label: 'doubling compute pass',
+  });
+  pass.setPipeline(pipeline);
+  pass.setBindGroup(0, bindGroup);
+  pass.dispatchWorkgroups(input.length);
+  pass.end();
+
+  encoder.copyBufferToBuffer(workBuffer, 0, resultBuffer, 0, resultBuffer.size);
+
+  const commandBuffer = encoder.finish();
+  device.queue.submit([commandBuffer]);
+
+  await resultBuffer.mapAsync(GPUMapMode.READ);
+  const result = new Float32Array(resultBuffer.getMappedRange().slice());
+  resultBuffer.unmap();
+
+  console.log('input', input);
+  console.log('result', result);
+}
+
 
 // setupCounter(document.querySelector('#counter'))
