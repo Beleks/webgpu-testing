5addc2ab4a6728802515fff435d97570ae4bea32
Aleksey Beletsky <53120159+Beleks@users.noreply.github.com>
2025-05-05 23:32:56 +0800

    Resize canvas

--- src/main.js
+++ src/main.js
@@ -43,6 +43,18 @@ getDevice()
     const pipelineRender = initRenderPipeline(device, module);
     const pipelineCompute = initComputePipeline(device, moduleCompute);
 
+    const observer = new ResizeObserver(entries => {
+      for (const entry of entries) {
+        const canvas = entry.target;
+        const width = entry.contentBoxSize[0].inlineSize;
+        const height = entry.contentBoxSize[0].blockSize;
+        canvas.width = Math.max(1, Math.min(width, device.limits.maxTextureDimension2D));
+        canvas.height = Math.max(1, Math.min(height, device.limits.maxTextureDimension2D));
+      }
+      render(device, pipelineRender);
+    });
+    observer.observe(canvas);
+
     render(device, pipelineRender);
     renderCompute(device, pipelineCompute);
   })
